5dc796b9f4f7bedb3e2defc242ff32ee39b9a471
Amit P Naik <amit.naik8103@gmail.com>
2020-11-07 22:04:13 +0530

    test: added test project

---
+++ src/HA.Adapter.Persistence.Unit.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace HA.Adapter.Persistence.Unit.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
