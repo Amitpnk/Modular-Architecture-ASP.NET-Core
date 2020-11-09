112d8d0b2e401cba961727edb5896b62003eee89
Amit P Naik <amit.naik8103@gmail.com>
2020-11-10 00:22:35 +0530

    fix: added appsetting to database migration proj

--- src/HA.DatabaseMigration/Startup.cs
+++ src/HA.DatabaseMigration/Startup.cs
@@ -1,4 +1,5 @@
 using HA.Adapter.Persistence;
+using HA.Domain.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -10,21 +11,19 @@ namespace HA.DatabaseMigration
 {
     public class Startup
     {
-        private readonly IConfigurationRoot configRoot;
         public IConfiguration Configuration { get; }
+        private AppSettings AppSettings { get; set; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            IConfigurationBuilder builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
-            configRoot = builder.Build();
+
+            AppSettings = new AppSettings();
+            Configuration.Bind(AppSettings);
         }
-
-
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddPersistence(Configuration, configRoot);
-
+            services.AddPersistence(Configuration, AppSettings);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
