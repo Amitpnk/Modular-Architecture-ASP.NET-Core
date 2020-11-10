c44302d4fd3d4e94c85d9576f2006b5c169aba89
Amit P Naik <amit.naik8103@gmail.com>
2020-11-11 00:05:36 +0530

    test: persistence layer

---
+++ src/HA.Adapter.Persistence.Unit.Test/Context/ApplicationDbContextTest.cs
@@ -0,0 +1,39 @@
+﻿using HA.Adapter.Persistence.Context;
+using HA.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+
+namespace HA.Adapter.Persistence.Unit.Test.Context
+{
+    public class ApplicationDbContextTest
+    {
+        [Test]
+        public void CanInsertDealIntoDatabasee()
+        {
+            using var context = new ApplicationDbContext();
+            var deal= new Deal();
+            context.Deals.Add(deal);
+            Assert.AreEqual(EntityState.Added, context.Entry(deal).State);
+        }
+
+        [Test]
+        public void CanUpdateDealIntoDatabasee()
+        {
+            using var context = new ApplicationDbContext();
+            var deal = new Deal();
+            context.Deals.Update(deal);
+            Assert.AreEqual(EntityState.Added, context.Entry(deal).State);
+        }
+
+        [Test]
+        public void CanDeleteDealIntoDatabasee()
+        {
+            using var context = new ApplicationDbContext();
+            var deal = new Deal();
+            context.Deals.Remove(deal);
+            Assert.AreEqual(EntityState.Deleted, context.Entry(deal).State);
+        }
+
+    }
+}
