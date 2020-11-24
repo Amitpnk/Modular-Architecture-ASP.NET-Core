2f450753eda6f14f41ff24a1d9d2e9c65170e5b4
Amit P Naik <amit.naik8103@gmail.com>
2020-11-25 01:49:48 +0530

    feat: Health check and Miniprofiler

--- src/HA.WebAPI/Startup.cs
+++ src/HA.WebAPI/Startup.cs
@@ -2,10 +2,14 @@ using HA.Adapter.DealModule;
 using HA.Adapter.Persistence;
 using HA.Application;
 using HA.Domain.Services;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
@@ -38,6 +42,10 @@ namespace HA.WebAPI
 
             services.AddPersistence(Configuration, AppSettings);
 
+            services.AddMemoryCache();
+
+            services.AddMiniProfiler(options => options.RouteBasePath = "/profiler").AddEntityFramework();
+
             // Todo: move to infrastucture layer
             services.AddSwaggerGen(setupAction =>
             {
@@ -64,6 +72,15 @@ namespace HA.WebAPI
                 var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
                 setupAction.IncludeXmlComments(xmlCommentsFullPath);
             });
+
+            services.AddHealthChecks()
+              .AddUrlGroup(new Uri(AppSettings.ApplicationDetail.ContactWebsite), name: "My personal website", failureStatus: HealthStatus.Degraded)
+              .AddSqlServer(Configuration.GetConnectionString("HexaArchConn"));
+
+            services.AddHealthChecksUI(setupSettings: setup =>
+            {
+                setup.AddHealthCheckEndpoint("Basic Health Check", $"/healthz");
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory log)
@@ -85,6 +102,23 @@ namespace HA.WebAPI
                 setupAction.RoutePrefix = "OpenAPI";
             });
 
+            app.UseMiniProfiler();
+
+            app.UseHealthChecks("/healthz", new HealthCheckOptions
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+                ResultStatusCodes =
+                {
+                    [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                    [HealthStatus.Degraded] = StatusCodes.Status500InternalServerError,
+                    [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable,
+                },
+            }).UseHealthChecksUI(setup =>
+            {
+                setup.ApiPath = "/healthcheck";
+                setup.UIPath = "/healthcheck-ui";
+            });
 
             app.UseHttpsRedirection();
 
